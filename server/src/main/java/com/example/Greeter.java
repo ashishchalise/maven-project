5751e706a789038b2cb4e8578fe4bb9a45c3e41e
Atul <atul.krbhatia@gmail.com>
2017-05-26 00:18:40 +0530

    commit a new method

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -20,4 +20,8 @@ public class Greeter {
   public String greet2(String someone) {
 	    return String.format("Hello USA , %s!", someone);
 	  }
+  
+  public String greet3(String someone) {
+	    return String.format("Hello India , %s!", someone);
+	  }
 }
